5d6d6f9b0a2f4e5bcac5b3802d178f16cacb18bd
Paweł <p.pawlik@itmcode.pl>
2024-12-08 17:52:45 +0100

    added api

---
+++ API/API/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<FileEntity> Files { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AppUser>()
+        .Property(u => u.Id)
+        .ValueGeneratedOnAdd();
+
+          
+        }
+    }
+}
